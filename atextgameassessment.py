0e557cb05eaa25f9c1674b252d6fcb2d54d5b84c
Oliver MacPherson <olivermacpherson999@gmail.com>
2014-10-20 03:52:25 +0000

    DaBoss

--- atextgameassessment.py
+++ atextgameassessment.py
@@ -1,7 +1,14 @@
 #Created by Oli MacPherson and Ben O'Sullivan!
+#For use with only 3 at the moment cause raw_input sucks
 
 import random
 import time
 
-def designbrief ():
-  
+def designbrief():
+    print("The International Foundation for the Prevention of Global Warming (IFPGW), a non-profit, non-governmental organisation, has asked you to develope a game that educates people about the risk of global warming, specifically the extreme weather changes that will occur if global warming is allowed to continue. As this game is for 13 - 16 year olds, it is to be suitably exaggerated to show these weather fluctuations. /n This game is to be created in Python 3.3, and is to show extreme rain and extreme drought. The IFPGW asks for this game to be submtted to their tester, Ms Katherine Young, in 3 weeks time, on thursday  ")
+
+designbrief()
+
+
+#BEGIN GAME!!!!!
+
